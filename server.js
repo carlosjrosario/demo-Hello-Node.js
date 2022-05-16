56cf1dbaa0381a24acc5f1be4e6aa661adcfaea9
carlosjrosario <carlosjrosario@gmail.com>
2022-05-16 16:28:44 -0400

    Actualizacion del Server

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const http = require('http');
 
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req, res) => 
+{
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
 
-server.listen(port, () => {
+server.listen(port, () => 
+{
   console.log(`Server running on port: ${port}`);
 });
